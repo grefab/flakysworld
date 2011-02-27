4615e54a8253e93109d7baf6102b0630d3b198d2
Gregor Fabritius <gre@g0r.de>
2011-02-27 01:12:36 +0100

    cleaned up main().

--- main.cpp
+++ main.cpp
@@ -6,29 +6,23 @@
 #include "polygonbodyview.h"
 #include "eyeview.h"
 
-int main(int argc, char *argv[])
+Surface* setupGUI(Universe* universe)
 {
-	QApplication app(argc, argv);
-	qsrand(time(0));
+	Surface* surface = new Surface();
+	surface->show();
 
-	Surface surface;
-	surface.show();
-
-	/* our engine! it takes care for the bodies we give to it. */
-	Universe* universe = new Universe();
-
-	QObject::connect(universe->engine(), SIGNAL(updatedFPS(int)), surface.fpsLabel(), SLOT(setNum(int)));
+	QObject::connect(universe->engine(), SIGNAL(updatedFPS(int)), surface->fpsLabel(), SLOT(setNum(int)));
 
 	/* build a corresponding view for each body. */
 	foreach(Body* body, universe->world()->bodies()) {
 		CircleBody* circleBody = dynamic_cast<CircleBody*>(body);
 		if ( circleBody ) {
-			surface.scene()->addItem( new CircleBodyView(*circleBody) );
+			surface->scene()->addItem( new CircleBodyView(*circleBody) );
 		}
 
 		PolygonBody* polygonBody = dynamic_cast<PolygonBody*>(body);
 		if ( polygonBody ) {
-			surface.scene()->addItem( new PolygonBodyView(*polygonBody) );
+			surface->scene()->addItem( new PolygonBodyView(*polygonBody) );
 		}
 	}
 
@@ -36,21 +30,38 @@ int main(int argc, char *argv[])
 	foreach(Sensor* sensor, universe->flaky()->sensors()) {
 		Eye* eye = dynamic_cast<Eye*>(sensor);
 		if ( eye ) {
-			surface.scene()->addItem( new EyeView(*eye) );
+			surface->scene()->addItem( new EyeView(*eye) );
 		}
 	}
 
 	/* show everything */
-	surface.view()->fitInView(surface.scene()->sceneRect(), Qt::KeepAspectRatio);
+	surface->view()->fitInView(surface->scene()->sceneRect(), Qt::KeepAspectRatio);
 
 	/* connect our key events */
-	QObject::connect(surface.view(), SIGNAL(keyPressed(Qt::Key)), universe, SLOT(keyPressHandler(Qt::Key)));
-	QObject::connect(surface.view(), SIGNAL(keyReleased(Qt::Key)), universe, SLOT(keyReleaseHandler(Qt::Key)));
+	QObject::connect(surface->view(), SIGNAL(keyPressed(Qt::Key)), universe, SLOT(keyPressHandler(Qt::Key)));
+	QObject::connect(surface->view(), SIGNAL(keyReleased(Qt::Key)), universe, SLOT(keyReleaseHandler(Qt::Key)));
 
 	/* and our engage button */
 	// TODO: Just provide motoric information and let flaky handle the consequences.
-	QObject::connect(&surface, SIGNAL(engageTriggered(qreal,qreal)), universe->flaky(), SLOT(accelerate(qreal,qreal)));
+	QObject::connect(surface, SIGNAL(engageTriggered(qreal,qreal)), universe->flaky(), SLOT(accelerate(qreal,qreal)));
+
+	return surface;
+}
+
+int main(int argc, char *argv[])
+{
+	QApplication app(argc, argv);
+	qsrand(time(0));
+
+	/* our engine! it takes care for the bodies we give to it. */
+	Universe* universe = new Universe();
+
+	/* make everything visible */
+	Surface* surface = setupGUI(universe);
 
 	/* preparation is done. let if flow! */
 	return app.exec();
+
+	delete surface;
+	delete universe;
 }
