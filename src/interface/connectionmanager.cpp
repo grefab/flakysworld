c9caa42d4db543abbc6a37479d12cf3d74eb91f3
Gregor Fabritius <gre@g0r.de>
2011-06-02 01:17:35 +0200

    comments that describe our JSON communication format (that has yet to be implemented)

--- src/interface/connectionmanager.cpp
+++ src/interface/connectionmanager.cpp
@@ -1,5 +1,42 @@
 #include "connectionmanager.h"
 
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+JSON Communication Spec
+--------
+Note that the characters  {}[]":  used here mean the actual characters (map, array,
+string delimiters), whereas  ()  group elements as entity.  +*  mean “one or more”
+and “zero or more” of the previously described entity (where entities have to be
+comma separated).  |  describes a set of optional elements, from which one has to
+be chosen.
+
+
+These objects can be received by flakysworld:
+
+{ "type" : "register",
+  "concerns" : ( "sensors" | "world" ) }
+{ "type" : "unregister",
+  "concerns" : ( "sensors" | "world" ) }
+{ "type" : "actuatorinput",
+  "being" : "BEING_ID",
+  "actuators" : { ( "ACTUATOR_ID" : [ REAL* ] )+ } }
+
+
+If registered to corresponding updates, a client will recieve:
+
+{ "type" : "sensoroutput",
+  "being" : "BEING_ID",
+  "sensors" : { ( "SENSOR_ID" : [ REAL* ] )+ } }
+{ "type" : "thing",
+  "thing" : "THING_ID",
+  "shape" : [ ( { "x" : REAL, "y" : REAL } )* ],
+  "position" : { "x" : REAL, "y" : REAL },
+  "rotation" : REAL }
+
+
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+
 ConnectionManager::ConnectionManager(Universe* universe, QObject *parent) :
 	QThread(parent),
 	universe_(universe)
@@ -60,6 +97,15 @@ void ConnectionManager::disconnected(QTcpSocket* socket)
 
 void ConnectionManager::dataArrived(QTcpSocket* socket, QVariant data)
 {
+	/* a socket can register itself for being notified about
+	 * - sensor updates
+	 * - world changes
+	 *
+	 * furthermore any connected socket is allowed to send neuron updates which
+	 * will be forwarded to the according being.
+	 */
+
+
 	/* handle incoming actuator data */
 	QString beingId;
 	QString actuatorId;
